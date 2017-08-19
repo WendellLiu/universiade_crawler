d5ca6862c8dbf4c11db28ceacb4974eff3a2f8c3
Wendell Liu <cuk.bas@gmail.com>
2017-08-20 01:32:48 +0800

    store data

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ from script import schedule
 
 data = schedule.main()
 keys = data[0].keys()
-with open('test.csv', 'w') as output_file:
+with open('output/schedule.csv', 'w') as output_file:
     dict_writer = csv.DictWriter(output_file, keys)
     dict_writer.writeheader()
     dict_writer.writerows(data)
